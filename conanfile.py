f53f6a4795cf3b28a67b8eeba33e2ea60ad3959a
Péter Kardos <kardospeter1994@hotmail.com>
2023-08-06 15:05:54 +0200

    update to conan 2.0, add release 0.9.4

--- conanfile.py
+++ conanfile.py
@@ -1,53 +1,51 @@
-from conans import ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan import tools
+
 import os
 
 class DSPBBConan(ConanFile):
     name = "dspbb"
+
+    # Metadata
     license = "MIT"
     homepage = "https://github.com/petiaccja/DSPBB"
     url = "https://github.com/conan-io/conan-center-index/"
     description = "Modern library for digital signal processing."
     topics = ("digital-signal-processing", "iir-filters", "fir-filters", "filter-design", "math", "dsp", "signals", "statistics")
+
+    # Configuration
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
-    settings = "compiler"
 
     @property
     def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "apple-clang": 10,
-            "clang": 6,
-            "gcc": 7,
-            "Visual Studio": 16,
-        }
-
-    def configure(self):
-        if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, "17")
-
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version:
-            if tools.Version(self.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration("dspbb requires C++17, which your compiler does not support.")
-        else:
-            self.output.warn("dspbb requires C++17. Your compiler is unknown. Assuming it supports C++17.")
-
+        return os.path.join(self.source_folder, "source_subfolder")
+    
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("DSPBB-" + self.version, self._source_subfolder)
+        tools.files.get(
+            self,
+            **self.conan_data["sources"][self.version],
+            destination=self._source_subfolder,
+            strip_root=True
+        )
 
     def requirements(self):
         for req in self.conan_data["requirements"][self.version]:
             self.requires(req)
             
     def package(self):
-        self.copy("*.hpp", dst=os.path.join("include", "dspbb"), src=os.path.join(self._source_subfolder, "include", "dspbb"))
-        self.copy("*.h", dst=os.path.join("include", "dspbb"), src=os.path.join(self._source_subfolder, "include", "dspbb"))
-        self.copy("LICENSE.md", dst="licenses", src=self._source_subfolder)
+        src_folder = os.path.join(self._source_subfolder, "include")
+        dst_folder = os.path.join(self.package_folder, "include")
+        lic_folder = os.path.join(self.package_folder, "licenses")
+
+        tools.files.copy(self, "*.hpp", dst=dst_folder, src=src_folder)
+        tools.files.copy(self, "*.h", dst=dst_folder, src=src_folder)
+        tools.files.copy(self, "LICENSE.md", dst=lic_folder, src=self._source_subfolder)
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,9 +1,19 @@
-from conans import ConanFile, CMake, tools
 import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import can_run
+
 
 class DSPBBTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
@@ -11,6 +21,6 @@ class DSPBBTestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(bin_path, env="conanrun")
